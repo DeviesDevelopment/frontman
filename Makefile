ee53ed98e15778d0e61efe41d8717e4a5e5f0e2a
Rickard Andersson <dunklas@gmail.com>
2022-01-28 07:08:12 +0100

    Issue a separate certbot certonly request for each https domain in servers.json
    I think this makes sense since there might be completely different domain names in servers.json. While I was attempting with hiveshop.rdunk.net and alamen.rdunk.net, certbot suggested to expand my current certificate to cover both domains. I think it's meant for cases where you want to use the same certificate for e.g. example.com, www.example.com

--- Makefile
+++ Makefile
@@ -12,17 +12,17 @@ validate-certs:
 	python3 validate-certs.py
 
 generate-certs:
-	MISSING_CERTS=$$(python3 print-missing-certs.py); \
-	if [ -z "$$MISSING_CERTS" ]; then \
-		echo "Found no certificates to generate"; \
-	else \
+	HTTPS_DOMAINS=$$(python3 print-https-certs.py); \
+	for domain in $${HTTPS_DOMAINS//,/ }; \
+	do \
 		docker-compose run --service-ports certbot certonly \
-			--domains "$$MISSING_CERTS" \
+			--domains "$$domain" \
+			--cert-name "$$domain" \
 			--non-interactive \
 			--standalone \
 			--agree-tos \
 			--register-unsafely-without-email; \
-	fi;
+	done \
 
 renew-certs:
 	docker-compose run --service-ports certbot renew
--- cert_utils.py
+++ cert_utils.py
@@ -10,9 +10,12 @@ def cert_missing(domain_name):
     private_exists = os.path.exists("/etc/letsencrypt/live/{}/privkey.pem".format(domain_name))
     return not (chain_exists and private_exists)
 
-def missing_certs():
+def https_domains():
     with open("servers.json") as f:
         servers_input = json.loads(f.read())
         https_servers = filter(is_https, servers_input)
         https_server_names = map(lambda https_server: https_server['server_name'], https_servers)
-        return list(filter(cert_missing, https_server_names))
+        return list(https_server_names)
+
+def missing_certs():
+    return list(filter(cert_missing, https_domains()))
