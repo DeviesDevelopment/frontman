76ca98c7f770ab7c96ee7a6b0a0c22d258218a4b
Fredrik Mile <fredrik.mile@gmail.com>
2022-02-15 21:33:11 +0100

    add negative e2e test

--- e2e/run-test.sh
+++ e2e/run-test.sh
@@ -5,8 +5,10 @@ cp servers.json ../servers.json
 cd ..
 make start
 
+# Try to reach service A endpoint in service A
+echo "Running Service A Test"
 A_RESPONSE=$(curl localhost:80/test-a --header "Host: service-a.com" --fail --silent)
-echo "Response: $A_RESPONSE"
+echo "Response from Service A: $A_RESPONSE"
 if [ "$A_RESPONSE" = "Hello from A" ]
 then
   echo "Test passed!"
@@ -15,9 +17,22 @@ else
   exit 1
 fi
 
+# Try to reach service B endpoint in service B
+echo "Running Service B Test"
 B_RESPONSE=$(curl localhost:80/test-b --header "Host: service-b.com" --fail --silent)
-echo "Response: $B_RESPONSE"
+echo "Response from Service B: $B_RESPONSE"
 if [ "$B_RESPONSE" = "Hello from B" ]
+then
+  echo "Test passed!"
+else
+  echo "Test failed"
+  exit 1
+fi
+
+# Try to reach service A endpoint in service B, should return 404.
+echo "Running Negative Test"
+NEGATIVE_RESPONSE_CODE=$(curl --write-out '%{http_code}' localhost:80/test-b --header "Host: service-a.com" --fail --silent)
+if [ "$NEGATIVE_RESPONSE_CODE" = 404 ]
 then
   echo "Test passed!"
 else
